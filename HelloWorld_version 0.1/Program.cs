df42c1f71cca8f8fe7433c72bc23ddc768840116
rin4ik <rin4ik@DESKTOP-902DGGL>
2020-10-08 22:27:28 +0300

    Добавил еще одну строку

---
+++ HelloWorld_version 0.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld_version_0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+         
+        }
+    }
+}
